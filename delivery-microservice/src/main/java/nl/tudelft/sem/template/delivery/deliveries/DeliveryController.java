3c17244bfbce31960487c4723cf33fa3a8c7f2cd
blytkowski <b.lytkowski@student.tudelft.nl>
2023-12-08 16:27:26 +0100

    Changed the way DeliveryRepository mock is created. Now it is instantiated using Mockito.

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/deliveries/DeliveryController.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/deliveries/DeliveryController.java
@@ -4,7 +4,10 @@ import nl.tudelft.sem.template.api.DeliveriesApi;
 import nl.tudelft.sem.template.model.Delivery;
 import nl.tudelft.sem.template.model.DeliveryStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
@@ -17,12 +20,14 @@ public class DeliveryController implements DeliveriesApi {
         this.deliveryDao = deliveryDao;
     }
 
+    // TODO: Authenticate user id
     @Override
     public ResponseEntity<String> deliveriesDeliveryIdStatusGet(@PathVariable UUID deliveryId, @RequestHeader String userId) {
         DeliveryStatus status = deliveryDao.getDeliveryStatus(deliveryId);
         return ResponseEntity.ok(status.toString());
     }
 
+    // TODO: Authenticate user id
     @Override
     public ResponseEntity<Delivery> deliveriesDeliveryIdStatusPut(@PathVariable UUID deliveryId, @RequestHeader String userId, @RequestBody String statusString) {
         DeliveryStatus status = DeliveryStatus.fromValue(statusString);
