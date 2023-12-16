2a9de1427421d5e9f9e7a800f5bcd31a7b85bb3c
LiliSitanyi <L.Sitanyi@student.tudelft.nl>
2023-12-16 11:05:20 +0100

    Statistics for deliveries per hour

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/controllers/StatisticsController.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/controllers/StatisticsController.java
@@ -1,17 +1,18 @@
 package nl.tudelft.sem.template.delivery.controllers;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import nl.tudelft.sem.template.api.StatisticsApi;
 import nl.tudelft.sem.template.delivery.communication.UsersCommunication;
 import nl.tudelft.sem.template.delivery.services.StatisticsService;
+import nl.tudelft.sem.template.model.Delivery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.time.OffsetDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RequestMapping("/statistics")
 @RestController
@@ -68,4 +69,69 @@ public class StatisticsController implements StatisticsApi {
   )
   default ResponseEntity<List<Integer>> statisticsRatingsForOrdersGet(@Parameter(name = "orderIds",description = "Array of order ids",required = true,in = ParameterIn.HEADER) @RequestHeader(value = "orderIds",required = true) @NotNull List<UUID> orderIds) {
     * */
+
+    /*
+    * @RequestMapping(
+        method = RequestMethod.GET,
+        value = "/statistics/deliveries-per-hour",
+        produces = { "application/json" }
+    )
+    default ResponseEntity<List<Integer>> statisticsDeliveriesPerHourGet(
+        @Parameter(name = "userId", description = "User ID for authorization", in = ParameterIn.HEADER) @RequestHeader(value = "userId", required = false) String userId
+    ) {
+    * */
+
+    /**
+     * Get a list of integers representing number of deliveries per hour
+     * @param userId User ID for authorization (optional)
+     * @return list of number of deliveries per hour (limiting to most recent day)
+     */
+    @Override
+    public ResponseEntity<List<Integer>> statisticsDeliveriesPerHourGet(@Parameter String userId) {
+        if (isNullOrEmpty(userId)) {
+            return ResponseEntity.badRequest().build();
+        }
+        String type = usersCommunication.getAccountType(userId);
+        if (!type.equals("vendor") && !type.equals("admin")) {
+            return ResponseEntity.badRequest().build();
+        }
+        List<Delivery> deliveries = statisticsService.getOrdersOfAVendor(userId);
+        if (deliveries == null || deliveries.isEmpty()) {
+            return ResponseEntity.ok(new ArrayList<>());
+        }
+        OffsetDateTime max = deliveries.get((deliveries.size()-1)).getDeliveredTime();
+        List<Delivery> mostRecent = deliveries.stream().filter(d -> d.getDeliveredTime().isAfter(max.minusHours(24)))
+            .sorted(Comparator.comparing(Delivery::getDeliveredTime)).collect(Collectors.toList());
+
+        int minHr = mostRecent.get(0).getDeliveredTime().getHour();
+        int maxHr = mostRecent.get((mostRecent.size()-1)).getDeliveredTime().getHour();
+        int n = (maxHr - minHr) + 1;
+        List<Integer> count = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            count.add(0);
+        }
+
+        int j = 0;
+        int currHr = minHr;
+        for (int i = 0; i < mostRecent.size(); i++) {
+            if (mostRecent.get(i).getDeliveredTime().getHour() < currHr) {
+                int currCount = count.get(j);
+                count.set(j, currCount+1);
+            } else {
+                j += 1;
+                currHr += 1;
+            }
+        }
+        return ResponseEntity.ok(count);
+    }
+
+    /**
+     * Checks if a string is null or empty
+     * @param str string to check
+     * @return boolean value indicating whether string is empty or not
+     */
+    public boolean isNullOrEmpty(String str) {
+        return str == null || str.isEmpty() || str.equals(" ");
+    }
 }
--- delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/services/StatisticsService.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/services/StatisticsService.java
@@ -2,11 +2,15 @@ package nl.tudelft.sem.template.delivery.services;
 
 import nl.tudelft.sem.template.delivery.domain.DeliveryRepository;
 import nl.tudelft.sem.template.model.Delivery;
+import nl.tudelft.sem.template.model.DeliveryStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class StatisticsService {
@@ -27,5 +31,17 @@ public class StatisticsService {
     public Integer getOrderRating(UUID deliveryId) {
         Optional<Delivery> delivery = deliveryRepository.findById(deliveryId);
         return delivery.map(x -> x.getRatingRestaurant()).orElseThrow(DeliveryService.DeliveryNotFoundException::new);
+    }
+
+    /**
+     * Get all fulfilled deliveries of a specific vendor
+     * @param userId vendor's email
+     * @return list of deliveries ordered by delivery time
+     */
+    public List<Delivery> getOrdersOfAVendor(String userId) {
+        List<Delivery> vendorDeliveries = deliveryRepository.findAll().stream()
+            .filter(d -> userId.equals(d.getRestaurantID())).collect(Collectors.toList());
+        List<Delivery> delivered = vendorDeliveries.stream().filter(d -> d.getStatus().equals(DeliveryStatus.DELIVERED)).collect(Collectors.toList());
+        return delivered.stream().sorted(Comparator.comparing(Delivery::getDeliveredTime)).collect(Collectors.toList());
     }
 }
