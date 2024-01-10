68720c6adddc8a31224bcbbcbe8c936efd74db50
Błażej Łytkowski <b.lytkowski@student.tudelft.nl>
2024-01-10 17:49:49 +0100

    Get order ids for customer

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/domain/DeliveryRepository.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/domain/DeliveryRepository.java
@@ -5,12 +5,13 @@ import nl.tudelft.sem.template.model.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
   List<Delivery> findAllByrestaurantID(String restaurantID);
+
+  List<Delivery> findAllByCustomerID(String customerID);
   List<Delivery> findAllByCourierID(String courierID);
-
-
 }
