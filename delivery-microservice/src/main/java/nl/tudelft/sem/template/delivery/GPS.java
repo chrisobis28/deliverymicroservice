9bf22fd7a54ea69773348542c198fbee20723be8
LiliSitanyi <L.Sitanyi@student.tudelft.nl>
2024-01-12 17:10:22 +0100

    changing if statements to switch cases to improve cyclomatic complexity (GPS)

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/GPS.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/GPS.java
@@ -12,8 +12,6 @@ import java.util.List;
 //Mock of Geocoding
 @Service
 public class GPS {
-
-  //List<Double> getCoordinatesOfAddress(List<String> address);
   List<Pair<List<Double>, String>> addresses = new ArrayList<>();
 
   List<Double> delft_coords = List.of(52.0115, 4.3586);
@@ -31,22 +29,23 @@ public class GPS {
     DecimalFormat df = new DecimalFormat("#.#####");
     DecimalFormat df2 = new DecimalFormat("##.#####");
     List<Double> coords;
+    Pair<List<Double>, String> p;
     double i = addresses.size();
-    if (location.contains("NL")) {
-      if (location.contains("Delft")) coords = delft_coords;
-      else if (location.contains("Rotterdam")) coords = rotterdam_coords;
-      else if (location.contains("Amsterdam")) coords = amsterdam_coords;
-      else coords = eindhoven_coords;
 
-      Pair<List<Double>, String> p = Pair.of(List.of(Double.parseDouble(df2.format((coords.get(0)+(i/100000)))), Double.parseDouble(df.format((coords.get(1)+(i/100000))))), location);
-      addresses.add(p);
-      return p;
+    if (location.contains("NL")) {
+      switch (location.split(" ")[2]) {
+        case ("Delft") -> coords = delft_coords;
+        case ("Rotterdam") -> coords = rotterdam_coords;
+        case ("Amsterdam") -> coords = amsterdam_coords;
+        default -> coords = eindhoven_coords;
+      }
+      p = Pair.of(List.of(Double.parseDouble(df2.format((coords.get(0)+(i/100000)))), Double.parseDouble(df.format((coords.get(1)+(i/100000))))), location);
     } else {
       df = new DecimalFormat("##.###");
-      Pair<List<Double>, String> p = Pair.of(List.of(Double.parseDouble(df.format((other.get(0)+(i/1000)))), Double.parseDouble(df.format((other.get(1)+(i/1000))))), location);
-      addresses.add(p);
-      return p;
+      p = Pair.of(List.of(Double.parseDouble(df.format((other.get(0)+(i/1000)))), Double.parseDouble(df.format((other.get(1)+(i/1000))))), location);
     }
+    addresses.add(p);
+    return p;
   }
 
   /**
@@ -59,15 +58,14 @@ public class GPS {
     if (coords.get(0) > 50.00) {
       int c = (int) (coords.get(0)*100000);
       int c2 = (int) (coords.get(1)*100000);
-      if (isInDelft(c, c2)) {
-        loc = addresses.get((c - 5201150)).getRight();
-      } else if (isInRotterdam(c, c2)) {
-        loc = addresses.get((c - 5192270)).getRight();
-      } else if (isInAmsterdam(c, c2)) {
-        loc = addresses.get((c - 5232120)).getRight();
-      } else {
-        loc = addresses.get((c - 5143550)).getRight();
+      int index = isInNL(c, c2).indexOf(0);
+      switch (index) {
+        case 0 -> loc = addresses.get((c - 5201150)).getRight();
+        case 1 -> loc = addresses.get((c - 5192270)).getRight();
+        case 2 -> loc = addresses.get((c - 5232120)).getRight();
+        default -> loc = addresses.get((c - 5143550)).getRight();
       }
+
     } else {
       int c = (int) (coords.get(0)*1000);
       loc = addresses.get((c - 39000)).getRight();
@@ -76,32 +74,33 @@ public class GPS {
   }
 
   /**
+   * Checks if coordinates are in NL
+   * @param c check value (from latitude coordinate)
+   * @param c2 check value (from longitude coordinate)
+   * @return list of values indicating if coordinates are in NL
+   */
+  public List<Integer> isInNL(int c, int c2) {
+    int check = (c - 5232120) == (c2 - 497070) && (c - 5232120) < addresses.size()?0:1;
+    return List.of(isInDelft(c, c2), isInRotterdam(c, c2), check);
+  }
+
+  /**
    * Checks if coordinates are in Delft
    * @param c check value (from latitude coordinate)
    * @param c2 check value (from longitude coordinate)
-   * @return boolean indicating if coordinates are in Delft
+   * @return integer value indicating if coordinates are in Delft
    */
-  public boolean isInDelft(int c, int c2) {
-    return (c - 5201150) == (c2 - 435860) && (c - 5201150) < addresses.size();
+  public int isInDelft(int c, int c2) {
+    return (c - 5201150) == (c2 - 435860) && (c - 5201150) < addresses.size()?0:1;
   }
 
   /**
    * Checks if coordinates are in Rotterdam
    * @param c check value (from latitude coordinate)
    * @param c2 check value (from longitude coordinate)
-   * @return boolean indicating if coordinates are in Rotterdam
+   * @return integer value indicating if coordinates are in Rotterdam
    */
-  public boolean isInRotterdam(int c, int c2) {
-    return (c - 5192270) == (c2 - 447920) && (c - 5192270) < addresses.size();
-  }
-
-  /**
-   * Checks if coordinates are in Amsterdam
-   * @param c check value (from latitude coordinate)
-   * @param c2 check value (from longitude coordinate)
-   * @return boolean indicating if coordinates are in Amsterdam
-   */
-  public boolean isInAmsterdam(int c, int c2) {
-    return (c - 5232120) == (c2 - 497070) && (c - 5232120) < addresses.size();
+  public int isInRotterdam(int c, int c2) {
+    return (c - 5192270) == (c2 - 447920) && (c - 5192270) < addresses.size()?0:1;
   }
 }
