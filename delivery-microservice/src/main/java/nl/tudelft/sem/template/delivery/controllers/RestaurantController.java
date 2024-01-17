1c7d5262264ba16693447c18ee1ed776b5296fe6
LiliSitanyi <L.Sitanyi@student.tudelft.nl>
2024-01-17 13:54:27 +0100

    Refactoring RestaurantController

--- delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/controllers/RestaurantController.java
+++ delivery-microservice/src/main/java/nl/tudelft/sem/template/delivery/controllers/RestaurantController.java
@@ -1,8 +1,6 @@
 package nl.tudelft.sem.template.delivery.controllers;
 
 
-import java.util.Objects;
-
 import nl.tudelft.sem.template.api.RestaurantsApi;
 
 
@@ -24,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import static nl.tudelft.sem.template.delivery.services.UsersAuthenticationService.AccountType.*;
 
 
 @RestController
@@ -130,50 +127,50 @@ public class RestaurantController implements RestaurantsApi {
         return ResponseEntity.ok(r);
     }
 
-    /**
-     * The put method for updating the location of a restaurant.
-     *
-     * @param restaurantId ID of the Restaurant entity (required)
-     * @param userId       User ID for authorization (required)
-     * @param requestBody  Coordinates of the new location of the restaurant
-     * @return updated location of a restaurant
-     */
-    @Override
-    public ResponseEntity<Restaurant> restaurantsRestaurantIdLocationPut(@PathVariable String restaurantId,
-                                                                         @RequestHeader String userId,
-                                                                         @RequestBody List<Double> requestBody) {
-        Pair<HttpStatus, String> result = usersCommunication.checkUserAccessToRestaurant(userId, restaurantId, "Location");
-        if (!(result.getLeft()).equals(HttpStatus.OK)) {
-            throw new ResponseStatusException(result.getLeft(), result.getRight());
-        }
-        restaurantService.updateLocation(restaurantId, requestBody);
-        return ResponseEntity.ok(restaurantService.getRestaurant(restaurantId));
-    }
+//    /**
+//     * The put method for updating the location of a restaurant.
+//     *
+//     * @param restaurantId ID of the Restaurant entity (required)
+//     * @param userId       User ID for authorization (required)
+//     * @param requestBody  Coordinates of the new location of the restaurant
+//     * @return updated location of a restaurant
+//     */
+//    @Override
+//    public ResponseEntity<Restaurant> restaurantsRestaurantIdLocationPut(@PathVariable String restaurantId,
+//                                                                         @RequestHeader String userId,
+//                                                                         @RequestBody List<Double> requestBody) {
+//        Pair<HttpStatus, String> result = usersCommunication.checkUserAccessToRestaurant(userId, restaurantId, "Location");
+//        if (!(result.getLeft()).equals(HttpStatus.OK)) {
+//            throw new ResponseStatusException(result.getLeft(), result.getRight());
+//        }
+//        restaurantService.updateLocation(restaurantId, requestBody);
+//        return ResponseEntity.ok(restaurantService.getRestaurant(restaurantId));
+//    }
 
-    /**
-     * The put method for updating the delivery zone of a restaurant.
-     *
-     * @param restaurantId ID of the Restaurant entity (required)
-     * @param userId       User ID for authorization (required)
-     * @param requestBody  Radius of the new delivery zone of the Restaurant
-     * @return updated delivery zone of a restaurant
-     */
-    @Override
-    public ResponseEntity<Restaurant> restaurantsRestaurantIdDeliverZonePut(@PathVariable String restaurantId,
-                                                                            @RequestHeader String userId,
-                                                                            @RequestBody Double requestBody) {
-        UsersAuthenticationService.AccountType accountType = usersCommunication.getUserAccountType(userId);
-        Pair<HttpStatus, String> result = usersCommunication.checkUserAccessToRestaurant(userId, restaurantId, "Delivery Zone");
-        if (!(result.getLeft()).equals(HttpStatus.OK)) {
-            throw new ResponseStatusException(result.getLeft(), result.getRight());
-        }
-        Restaurant r = restaurantService.getRestaurant(restaurantId);
-        if (accountType.equals(VENDOR) && r.getCouriers().isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User lacks necessary permissions.");
-        }
-        restaurantService.updateDeliverZone(restaurantId, requestBody);
-        return ResponseEntity.ok(restaurantService.getRestaurant(restaurantId));
-    }
+//    /**
+//     * The put method for updating the delivery zone of a restaurant.
+//     *
+//     * @param restaurantId ID of the Restaurant entity (required)
+//     * @param userId       User ID for authorization (required)
+//     * @param requestBody  Radius of the new delivery zone of the Restaurant
+//     * @return updated delivery zone of a restaurant
+//     */
+//    @Override
+//    public ResponseEntity<Restaurant> restaurantsRestaurantIdDeliverZonePut(@PathVariable String restaurantId,
+//                                                                            @RequestHeader String userId,
+//                                                                            @RequestBody Double requestBody) {
+//        UsersAuthenticationService.AccountType accountType = usersCommunication.getUserAccountType(userId);
+//        Pair<HttpStatus, String> result = usersCommunication.checkUserAccessToRestaurant(userId, restaurantId, "Delivery Zone");
+//        if (!(result.getLeft()).equals(HttpStatus.OK)) {
+//            throw new ResponseStatusException(result.getLeft(), result.getRight());
+//        }
+//        Restaurant r = restaurantService.getRestaurant(restaurantId);
+//        if (accountType.equals(VENDOR) && r.getCouriers().isEmpty()) {
+//            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User lacks necessary permissions.");
+//        }
+//        restaurantService.updateDeliverZone(restaurantId, requestBody);
+//        return ResponseEntity.ok(restaurantService.getRestaurant(restaurantId));
+//    }
 
     /**
      * The get method for getting the orders of a restaurant that were not assigned yet.
@@ -203,35 +200,35 @@ public class RestaurantController implements RestaurantsApi {
         }
     }
 
-    /**
-     * Sets the list of couriers and returns the correct response codes.
-     *
-     * @param restaurantId ID of the Restaurant entity (required)
-     * @param userId       User ID for authorization (required)
-     * @param requestBody  Put a preferred set of couriers for the restaurant (required)
-     * @return the modified restaurant entity
-     */
-    @Override
-    public ResponseEntity<Restaurant> restaurantsRestaurantIdCouriersPut(@PathVariable String restaurantId,
-                                                                         @RequestHeader String userId,
-                                                                         @RequestBody @Valid List<String> requestBody) {
-        Pair<HttpStatus, String> result = usersCommunication.checkUserAccessToRestaurant(userId, restaurantId, "Couriers");
-        if (!(result.getLeft()).equals(HttpStatus.OK)) {
-            throw new ResponseStatusException(result.getLeft(), result.getRight());
-        }
-        // check couriers
-        if (requestBody != null) {
-            for (String id : requestBody) {
-                UsersAuthenticationService.AccountType account = usersCommunication.getUserAccountType(id);
-                if (!Objects.equals(account, COURIER)) {
-                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                        "List contains the id of someone who isn't a courier.");
-                }
-            }
-        }
-        Restaurant r = restaurantService.setListOfCouriers(restaurantId, requestBody);
-        return ResponseEntity.ok(r);
-    }
+//    /**
+//     * Sets the list of couriers and returns the correct response codes.
+//     *
+//     * @param restaurantId ID of the Restaurant entity (required)
+//     * @param userId       User ID for authorization (required)
+//     * @param requestBody  Put a preferred set of couriers for the restaurant (required)
+//     * @return the modified restaurant entity
+//     */
+//    @Override
+//    public ResponseEntity<Restaurant> restaurantsRestaurantIdCouriersPut(@PathVariable String restaurantId,
+//                                                                         @RequestHeader String userId,
+//                                                                         @RequestBody @Valid List<String> requestBody) {
+//        Pair<HttpStatus, String> result = usersCommunication.checkUserAccessToRestaurant(userId, restaurantId, "Couriers");
+//        if (!(result.getLeft()).equals(HttpStatus.OK)) {
+//            throw new ResponseStatusException(result.getLeft(), result.getRight());
+//        }
+//        // check couriers
+//        if (requestBody != null) {
+//            for (String id : requestBody) {
+//                UsersAuthenticationService.AccountType account = usersCommunication.getUserAccountType(id);
+//                if (!Objects.equals(account, COURIER)) {
+//                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+//                        "List contains the id of someone who isn't a courier.");
+//                }
+//            }
+//        }
+//        Restaurant r = restaurantService.setListOfCouriers(restaurantId, requestBody);
+//        return ResponseEntity.ok(r);
+//    }
 
     /**
      * deletes a restaurant from the system.
